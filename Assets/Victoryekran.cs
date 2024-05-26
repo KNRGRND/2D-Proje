719848279f2a12fedc26be507ece7b7776e254b9
unknown <mustafakaner@outlook.com.tr>
2024-05-26 18:13:52 +0300

    victory ekranı eklendi

---
+++ Assets/Victoryekran.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Victoryekran : MonoBehaviour
+{
+    public void mainmenubutton()
+    {
+        SceneManager.LoadScene("girisEkran");
+    }
+
+}
